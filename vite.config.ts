f6f0a19fc031303cab94203705d5a54b40339c19
juliechang0520 <juliechang0520@gmail.com>
2025-12-18 14:11:42 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+        base: '/juliechang/', // <--- 加上這一行，必須與儲存庫名稱一致
       server: {
         port: 3000,
         host: '0.0.0.0',
